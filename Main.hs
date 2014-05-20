c5a8124577b554129d64a8e879b6efc12377d3c8
xanxys <xanxys@gmail.com>
2014-05-20 16:14:34 +0900

    Code cleanup (remove learning-related).

--- Main.hs
+++ Main.hs
@@ -1,11 +1,5 @@
-import System.Environment
 import Control.Monad
 
 import CUI
-import Learn
 
-main = do
-	args <- getArgs
-	case args of
-		["--learn"] -> doLearn
-		_ -> void $ doCUIGame
+main = void $ doCUIGame
